fbbcc86cd067959d948a18391d54bc37377be1cc
bidhanDas <264cat@gmail.com>
2024-03-27 16:37:21 +0600

    newwwwwww

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import styles from './page.module.css'
 
-// async function getData(){
-//   const res = await fetch(`${process.env.HOST}/api/category`,{cache:'no-cache'});
-//   return res.json();
-// }
+async function getData(){
+  const res = await fetch(`${process.env.HOST}/api/category`,{cache:'no-cache'});
+  return res.json();
+}
 
-const page = () => {
-  // const x =await getData();
+const page = async () => {
+  const x =await getData();
   return (
     <div>
       <h1>f u</h1>
-      {/* {x.data[0].name} */}
+      {x.data[0].name}
     </div>
   )
 }
